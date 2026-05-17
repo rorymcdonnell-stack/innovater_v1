0b4530f4672b67fbfdb44142cdb3c2569c2e4993
rorymcdonnell-stack <rorymcdonnell@me.com>
2026-05-17 11:09:21 +0100

    Adjust logo opacity in page component

--- innovater/app/page.tsx
+++ innovater/app/page.tsx
@@ -147,7 +147,7 @@ export default function Home() {
             { src: '/niq.png', alt: 'NIQ' },
             { src: '/azure.png', alt: 'Azure' },
           ].map(logo => (
-            <div key={logo.alt} style={{ opacity: 0.55, filter: 'brightness(0) invert(1)' }}>
+            <div key={logo.alt} style={{ opacity: 0.7 }}>
               <img src={logo.src} alt={logo.alt} style={{ height: '26px', width: 'auto', objectFit: 'contain' as const }} />
             </div>
           ))}
